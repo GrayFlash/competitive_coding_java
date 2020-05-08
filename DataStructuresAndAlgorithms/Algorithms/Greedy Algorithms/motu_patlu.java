eadc305045f92c2d19179d99bfa648c05363d55f
Gaurav-ECE-SVNIT <gk74533@gmail.com>
2020-05-08 21:10:55 +0530

    Some more codes

---
+++ DataStructuresAndAlgorithms/Algorithms/Greedy Algorithms/motu_patlu.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class motu_patlu {
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new Bufferedreader (new InputStrewamReader (Systeem.in));
+        int T = sc.nextInt();
+        while(T-->0){
+            int n = sc.nextInt();
+            int ar[] = new int[n+7];
+            for(int i=0; i<n ;i++){
+                ar[i] = sc.nextInt();
+            }
+            long motu_t=0, patlu_t=0;
+            int ice_cream = 0;
+            int i=0;
+            while(ice_cream<n){
+                motu_t = ar[i]; 
+            }
+        }
+    }
+}
